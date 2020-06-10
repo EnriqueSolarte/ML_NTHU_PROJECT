b578b0bb06e73ed2424a0b98bae7200ba9d4a4d8
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2020-06-10 20:12:35 +0800

    Init filters.py

---
+++ src/filters.py
@@ -0,0 +1,20 @@
+from setup import *
+from src.reading_data import *
+
+
+def laplacian_filter(src, kernel_size=3):
+    src = cv2.GaussianBlur(src, (11, 11), 0)
+    return cv2.Laplacian(src, cv2.CV_64F, ksize=kernel_size)
+
+
+def sobel_x_filter(src, kernel_size):
+    sobelx = cv2.Sobel(src, cv2.CV_64F, 1, 0, kernel_size)
+
+
+if __name__ == '__main__':
+    while True:
+        image = read_random_images_by_label(label=2, verbose=False)
+        cv2.imshow("Original Image", image)
+        cv2.imshow("Laplacian", laplacian_filter(image))
+        if cv2.waitKey(0) == 27:
+            break
