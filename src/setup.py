4cfe407d7fd9558c01dbf87a18967527d573ae9b
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2020-06-13 01:52:58 +0800

    defined classes list

--- src/setup.py
+++ src/setup.py
@@ -1,11 +1,10 @@
 import sys
 import os
 
-print("         ML COURSE FINAL PROJECT         ")
-print("                Fall 2020                ")
+print("ML COURSE FINAL PROJECT")
+print("NTHU Fall 2020")
 
 main_project = os.path.dirname(os.path.dirname(__file__))
-print(main_project)
 sys.path.append(main_project)
 print("\n# PYTHON PATH:")
 [print(path) for path in sys.path]
@@ -16,8 +15,9 @@ DATA_DIR = os.path.join(main_project, "data")
 DATA_TRAIN_DIR = os.path.join(main_project, "data", "train_images")
 DATA_TEST_DIR = os.path.join(main_project, "data", "test_images")
 SRC_DIR = os.path.join(main_project, "src")
-
 print("DATA_DIR: {}".format(DATA_DIR))
 print("DATA_TRAIN_DIR: {}".format(DATA_TRAIN_DIR))
 print("DATA_TEST_DIR: {}".format(DATA_TEST_DIR))
 print("SRC_DIR: {}".format(SRC_DIR))
+
+classes = ['normal', 'void', 'horizontal_defect', 'vertical_defect', 'edge_defect', 'particle']
