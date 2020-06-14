0fa0b3fb49d6dbec7b89ef52d877cd49e127198b
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2020-06-14 21:03:28 +0800

    refactoring data

--- src/Train.py
+++ src/Train.py
@@ -1,9 +1,9 @@
 import os
-import CNN_models
+import classifier_cnn
 
 
 # Create Alex net model
-model = CNN_models.AlexNetModel()
+model = classifier_cnn.AlexNetModel()
 
 # Visualize model
 model.summary()
@@ -16,16 +16,16 @@ batch_size = 5
 epochs = 2
 
 # Convert dataset into batches for training
-Train_batch,Test_batch,Val_batch = CNN_models.Images_into_batches(Train_dir, Test_dir, Val_dir, batch_size)
+Train_batch,Test_batch,Val_batch = classifier_cnn.Images_into_batches(Train_dir, Test_dir, Val_dir, batch_size)
 
 # Train the model
-model = CNN_models.model_train(model, Train_batch, Val_batch, epochs, batch_size)
+model = classifier_cnn.model_train(model, Train_batch, Val_batch, epochs, batch_size)
 
 # Predict  
-predicted = CNN_models.model_predict(model, Test_batch)
+predicted = classifier_cnn.model_predict(model, Test_batch)
 
 # Calculate accuracy
 test_csv_path = os.path.join(os.path.abspath('..'),'data/train.csv')# Path of testing csv
-accuracy = CNN_models.calculate_accuracy(predicted, test_csv_path)# check predicted and ground truth
+accuracy = classifier_cnn.calculate_accuracy(predicted, test_csv_path)# check predicted and ground truth
 print(accuracy)
     
